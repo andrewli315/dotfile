435f097dd1f2979e1e92adb9d0ba1d3115c5b799
andrewli315 <nyps70414@gmail.com>
2019-06-04 16:14:40 +0800

    ctf-tool-installation

---
+++ install.sh
@@ -0,0 +1,16 @@
+apt-get install vim -y
+apt-get install python -y
+apt-get install python-pip -y
+apt-get install python3 -y
+apt-get install ruby -y
+apt-get install gem -y
+apt-get install nmap -y
+python -m pip install pwntools --user
+gem install one_gadget
+
+git clone https://github.com/longld/peda.git ~/peda
+git clone https://github.com/scwuaptx/Pwngdb.git ~/Pwngdb
+cp ~/Pwngdb/.gdbinit ~/
+
+
+
